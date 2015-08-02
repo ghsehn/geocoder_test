8fd0e0e6d92b22f80e49cdc07d0a56702177b778
Guilherme Sehn <ghsehn@gmail.com>
2015-08-02 19:57:46 +0000

    added google maps url to listing

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ActiveRecord::Base
   geocoded_by :address       # can also be an IP address
   after_validation :geocode  # auto-fetch coordinates
+
+  def google_maps_url
+    "https://www.google.com.br/maps/?q=#{latitude},#{longitude}"
+  end
 end
